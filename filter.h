1653dad83e97b71209451053821651aeaab3f183
Oliver Michalowski <56884215+moliverxxii@users.noreply.github.com>
2020-12-05 13:46:54 +0100

    Add files via upload

---
+++ filter.h
@@ -0,0 +1,16 @@
+/*
+ * filter.h
+ *
+ *  Created on: Jan 15, 2020
+ *      Author: moliver
+ */
+
+#ifndef FILTER_H_
+#define FILTER_H_
+
+#include "main.h"
+
+void flou(image_t image, int width,int height);
+
+
+#endif /* FILTER_H_ */
