9d27d9896210f97b6b824fec118867c539f3c100
Oliver Michalowski <moliver2204@gmail.com>
2022-08-27 21:34:02 +0200

    3x3 filtre moyen saturant pour le rendu

--- sources/filter.c
+++ sources/filter.c
@@ -7,40 +7,47 @@
 
 #include "main.h"
 
-void flou(image_t* image)
+void flou(image_t *image)
 {
-    image_t* image2 = initImage(image->width,image->height);
+    image_t *image2 = initImage(image->width, image->height);
 
     int x;
     int y;
-    for(x=1; x<image->width-1; ++x )
+    int tmp;
+    color_struct_t *point;
+    for (x = 1; x < image->width - 1; ++x)
     {
-        for(y=1; y<image->height-1; ++y)
+        for (y = 1; y < image->height - 1; ++y)
         {
-            for(int i=-1;i<2;++i)
+            for (int i = -1; i < 2; ++i)
             {
-                for(int j=-1;j<2;++j)
+                for (int j = -1; j < 2; ++j)
                 {
-                    image2->image[x][y][0] |= image->image[x+i][y+j][0];
-                    image2->image[x][y][1] |= image->image[x+i][y+j][1];
-                    image2->image[x][y][2] |= image->image[x+i][y+j][2];
-                }
+                    for (int col = 0; col < 3; col++)
+                    {
+                        tmp = image2->image[x][y][col];
 
+                        image2->image[x][y][col] =
+                                saturator(
+                                        tmp
+                                        +((int)image->image[x + i][y  + j][col])/5,
+                                        SAT_MIN,
+                                        SAT_MAX);
+                    }
+                }
             }
         }
     }
 
-    for(x=0;x<image->width;++x)
+    for (x = 0; x < image->width; ++x)
     {
-        for(y=0;y<image->height;++y)
+        for (y = 0; y < image->height; ++y)
         {
-            image->image[x][y][0] |= image2->image[x][y][0];
-            image->image[x][y][1] |= image2->image[x][y][1];
-            image->image[x][y][2] |= image2->image[x][y][2];
+            point = (color_struct_t*) image->image[x][y];
+            *point = *((color_struct_t*) (image2->image[x][y]));
 
         }
     }
 //  freeImage(image2,width,height);
 }
-
 
