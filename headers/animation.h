4976de5e2133e398e31e085c26a291e975b8e26f
Oliver Michalowski <moliver2204@gmail.com>
2024-02-08 09:50:11 +0100

    Ajout des fichiers vides "animation.c/.h".

---
+++ headers/animation.h
@@ -0,0 +1,12 @@
+/*
+ * animation.h
+ *
+ *  Created on: 8 févr. 2024
+ *      Author: moliver
+ */
+
+#ifndef HEADERS_ANIMATION_H_
+#define HEADERS_ANIMATION_H_
+
+
+#endif /* HEADERS_ANIMATION_H_ */
---
+++ sources/animation.c
@@ -0,0 +1,8 @@
+/*
+ * animation.c
+ *
+ *  Created on: 8 févr. 2024
+ *      Author: moliver
+ */
+
+#include "animation.h"
