b625d5f03e2e322cf60d1f047ece125c1b0819c3
Oliver Michalowski <moliver2204@gmail.com>
2024-02-08 10:09:41 +0100

    Ajout des fichiers vides interprètes "interpreter.c/.h".

---
+++ headers/interpreter.h
@@ -0,0 +1,13 @@
+/*
+ * interpreter.h
+ *
+ *  Created on: 8 févr. 2024
+ *      Author: moliver
+ */
+
+#ifndef HEADERS_INTERPRETER_H_
+#define HEADERS_INTERPRETER_H_
+
+
+
+#endif /* HEADERS_INTERPRETER_H_ */
---
+++ sources/interpreter.c
@@ -0,0 +1,9 @@
+/*
+ * interpreter.c
+ *
+ *  Created on: 8 févr. 2024
+ *      Author: moliver
+ */
+
+
+#include "interpreter.h"
