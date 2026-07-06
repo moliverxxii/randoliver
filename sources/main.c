cc534114f332ba09f3f22cf1543736f0b2d9bfd2
Oliver Michalowski <moliver2204@gmail.com>
2026-07-06 14:34:06 +0200

    ajout des performances detaillee des tests

--- sources/main.c
+++ sources/main.c
@@ -39,19 +39,49 @@ main(int argc, char* argv[])
             = performance_init(preset_get_name(preset));
         performance_try_start(&performance_preset);
 
+        performance_t perf_init = performance_init("  init");
+        performance_try_start(&perf_init);
         preset_model_init(preset);
+        performance_try_add(&perf_init);
+
         uint32_t frame_count = preset_frame_count(preset);
+
+        performance_t perf_frame  = performance_init("  frame");
+        performance_t perf_update = performance_init("  update");
+        performance_t perf_render = performance_init("  render");
+        performance_t perf_file   = performance_init("  file");
+
         for(uint32_t frame = 0; frame < frame_count; ++frame)
         {
+            performance_try_start(&perf_frame);
+
+            performance_try_start(&perf_update);
             preset_model_update(preset);
+            performance_try_add(&perf_update);
+
             image_set(frame_p);
+            performance_try_start(&perf_render);
             preset_frame_render(preset, frame_p);
+            performance_try_add(&perf_render);
+
+            performance_try_start(&perf_file);
             image_file_write(preset_get_name(preset), frame_p, NULL);
+            performance_try_add(&perf_file);
         }
         preset_model_free(preset);
+
         performance_try_add(&performance_preset);
         performance_print(&performance_preset);
+        performance_print(&perf_init);
+        performance_print(&perf_update);
+        performance_print(&perf_render);
+        performance_print(&perf_file);
+
         performance_free(&performance_preset);
+        performance_free(&perf_init);
+        performance_free(&perf_update);
+        performance_free(&perf_render);
+        performance_free(&perf_file);
     }
     image_free(frame_p);
 
