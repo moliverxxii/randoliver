84ce224d87f83fb645ff0471d4b2a1084871ada4
Oliver Michalowski <moliver2204@gmail.com>
2026-07-18 19:34:29 +0200

    les animations incrementent les numeros de fichier et on a une conversion au format video

--- sources/main.c
+++ sources/main.c
@@ -1,4 +1,4 @@
-/*&cam
+/*
  * main.c
  *
  *  Created on: 13 févr. 2019
@@ -6,7 +6,9 @@
  */
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "image_file.h"
 #include "interface.h"
@@ -33,7 +35,7 @@ main(int argc, char* argv[])
     //interface_state_save();
     for(uint32_t preset = 0; preset < preset_get_total_count(); ++preset)
     {
-        image_t* frame_p = image_init(320,240);
+        image_t* frame_p = image_init(320,180);
 
         performance_t performance_preset
           = performance_init(preset_get_name(preset));
@@ -68,12 +70,67 @@ main(int argc, char* argv[])
             performance_try_start(&perf_file);
             if(exists_b)
             {
-                image_file_write(preset_get_name(preset), frame_p, NULL);
+                char* name_p = (char*) preset_get_name(preset);
+                if(frame_count > 1)
+                {
+                    name_p = file_name_extension_number(name_p, frame);
+                }
+                image_file_write(name_p, frame_p, NULL);
+                if(frame_count > 1)
+                {
+                    free(name_p);
+                }
             }
             performance_try_add(&perf_file);
 
             performance_try_add(&perf_frame);
         }
+        if(frame_count > 1)
+        {
+            const char* preset_name_p = preset_get_name(preset);
+
+            const char* const input_suffix_p  = " %d.bmp";
+            char* name_input_p = malloc((strlen(preset_name_p) + strlen(input_suffix_p) + 1) * sizeof(char));
+            strcpy(name_input_p, preset_name_p);
+            strcat(name_input_p, input_suffix_p);
+
+            const char* const output_suffix_p = ".mov";
+            char* name_output_p = malloc((strlen(preset_name_p) + strlen(output_suffix_p) + 1) * sizeof(char));
+            strcpy(name_output_p, preset_name_p);
+            strcat(name_output_p, output_suffix_p);
+
+            char frame_count_text[11];
+            sprintf(frame_count_text, "%u", frame_count);
+
+            pid_t process = fork();
+            if(0 == process)
+            {
+                int conversion_process = execlp("ffmpeg", "ffmpeg",
+                                                "-y",
+                                                "-v",       "warning",
+                                                "-f",       "image2",
+                                                "-r",       "10",
+                                                "-i",       name_input_p,
+                                                "-frames",  frame_count_text,
+                                                "-pix_fmt", "yuv420p",
+                                                name_output_p,
+                                                NULL);
+                if(conversion_process != 0)
+                {
+                    //pas de ffmpeg chez vous monsieur/madame!
+                    free(name_input_p);
+                    free(name_output_p);
+                    exit(0);
+                }
+
+            }
+            else
+            {
+                free(name_input_p);
+                free(name_output_p);
+            }
+        }
+
         preset_model_free(preset);
 
         performance_try_add(&performance_preset);
